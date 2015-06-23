af0562ce5f78a087da420d4a3da4679bbda66ee9
Shauvik Roy Choudhary <shauvik@gmail.com>
2015-06-23 14:53:10 -0400

    fix to consider instance class siblings only

--- src/main/java/com/checkdroid/crema/EspressoPlus.java
+++ src/main/java/com/checkdroid/crema/EspressoPlus.java
@@ -1,5 +1,23 @@
 /*
  * Copyright (c) 2015. CheckDroid at Georgia Tech
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 package com.checkdroid.crema;
@@ -132,7 +150,8 @@ public class EspressoPlus {
                             List<View> childNodeList = new ArrayList<>();
                             for(int i=0;i<currNodeParent.getChildCount();i++){
                                 View childNode = currNodeParent.getChildAt(i);
-                                if(childNode!=null && childNode.getVisibility()==View.VISIBLE){
+                                if(childNode!=null && childNode.getVisibility()==View.VISIBLE
+                                        && isInstance(childNode, sNodeClasses[index])){
                                     childNodeList.add(childNode);
                                 }
                             }
@@ -328,16 +347,7 @@ public class EspressoPlus {
                 return result;
             }
 
-            private boolean isInstance(Object o, String className) {
-                try {
-                    Class clazz = Class.forName(className);
-                    boolean result = clazz.isInstance(o);
-                    return result;
-                } catch (ClassNotFoundException cnfe) {
-                    Log.d(TAG, "isInstance: ClassNotFoundException");
-                    return false;
-                }
-            }
+
         };
     }
 
@@ -390,6 +400,17 @@ public class EspressoPlus {
             }
         }
         return xp;
+    }
+
+    private static boolean isInstance(Object o, String className) {
+        try {
+            Class clazz = Class.forName(className);
+            boolean result = clazz.isInstance(o);
+            return result;
+        } catch (ClassNotFoundException cnfe) {
+            Log.d(TAG, "isInstance: ClassNotFoundException");
+            return false;
+        }
     }
 
 }
